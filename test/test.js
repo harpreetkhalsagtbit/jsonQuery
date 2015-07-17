a1f580d9b03ed73846cb774b895071d02c7908e4
ksr89 <kuljeetginyu@gmail.com>
2015-07-17 13:51:21 +0530

    Add: Tests

--- test/test.js
+++ test/test.js
@@ -4,6 +4,7 @@ var should = require('chai').should(),
 
 var data = [{"name":"harpreet","age":25,"subjects":[{"name":"English","class":"8"},{"name":"Hindi","class":"8"},{"name":"Math","class":"8"},{"name":"Science","class":"8"}]},{"name":"kuljeet","age":26,"subjects":[{"name":"English","class":"12"},{"name":"Punjabi","class":"12"},{"name":"Math","class":"12"},{"name":"Science","class":"12"}]}]
 
+// Test Case 1
 describe('deepSearchForSpecificKey', function() {
   it('deep Search For Specific Key', function() {
     var _res = jsonQuery.query("$..[?class]", data)
@@ -11,6 +12,7 @@ describe('deepSearchForSpecificKey', function() {
   });
 });
 
+// Test Case 2
 describe('deepSearchForSpecificKeyAndSpecificValue', function() {
   it('deep Search For Specific Key And Specific Value', function() {
     var _res = jsonQuery.query("$..[?class=8]", data)
@@ -18,6 +20,7 @@ describe('deepSearchForSpecificKeyAndSpecificValue', function() {
   });
 });
 
+// Test Case 3
 describe('deepSearchForSpecificKeyAndSpecificValueHavingRange', function() {
   it('deep Search For Specific Key And Specific Value Having Range', function() {
     var _res = jsonQuery.query("$..[?age>25]", data)
